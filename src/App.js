6e4d063ea08e4b4b80db9bdd680de5eefe08e197
Mun Kit <leoan96@hotmail.com>
2021-08-16 13:58:41 +0800

    setup basic app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddFreelancer from "./components/add-freelancer.component";
+import Freelancer from "./components/freelancer.component";
+import FreelancerList from "./components/freelancer-list.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/freelancers" className="navbar-brand">
+          Complete Developer Network
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/freelancers"} className="nav-link">
+              Freelancers
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route
+            exact
+            path={["/", "/freelancers"]}
+            component={FreelancerList}
+          />
+          <Route exact path="/add" component={AddFreelancer} />
+          <Route path="/freelancer/:id" component={Freelancer} />
+        </Switch>
+      </div>
     </div>
   );
 }
